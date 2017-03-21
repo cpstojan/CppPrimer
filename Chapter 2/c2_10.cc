b4e0e9b89d85965713590a10806a4671ad5290db
Chris <christopher.stojanovski@gmail.com>
2017-03-20 23:52:17 -0400

    Exercises on intialization

---
+++ Chapter 2/c2_10.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// What are the initial values of each of the variables?
+
+std::string global_str; // empty string
+int global_int; // value of 0
+int main () {
+	int local_int; // uninitialized garbage value
+	std::string local_str; // empty string
+	return 0;
+}
