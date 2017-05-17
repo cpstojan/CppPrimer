fa4283dbe6360e2670998719f56212b26c442227
Chris <christopher.stojanovski@gmail.com>
2017-05-16 20:55:02 -0400

    Adding comments to make program more clear

--- Chapter 1/c1_10.cc
+++ Chapter 1/c1_10.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 
 int main () {
+	// Counting down from val to 0, storing addition in sum
 	int val = 10, sum = 0;
 	while (val > 0) {
 		sum += val;
 		--val;
 	}
+	// Printing output of total sum
 	std::cout << "The sum from 10 to 0 is: " << sum << std::endl;
 }
