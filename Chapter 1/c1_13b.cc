eacd9029296b885fc5e2eb469708bd55c1358bed
Chris <christopher.stojanovski@gmail.com>
2017-05-17 22:01:43 -0400

    Description of program

--- Chapter 1/c1_13b.cc
+++ Chapter 1/c1_13b.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main () {
+	// starting at from, up to to
+	// each number up to and including is summed
 	int from = 10, to = 0, sum = 0;
 	for (int n = from; n >= to; --n)
 		sum += n;
