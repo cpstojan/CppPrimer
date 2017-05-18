fdc67c33bdf77acaf4df38f04e18296c9d2933c2
Chris <christopher.stojanovski@gmail.com>
2017-05-17 21:56:39 -0400

    Comments to increase clarity

--- Chapter 1/c1_13a.cc
+++ Chapter 1/c1_13a.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main () {
+	// min is counted from, max is counted up to
+	// sum of numbers including and between min and max is saved in sum
 	int min = 50, max = 100, sum = 0;
 	for (int count = min; count <= max; ++count)
 		sum += count;
