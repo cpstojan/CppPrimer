5226065d3801b2ed815290bd8511cf957a6c6f0e
Chris <christopher.stojanovski@gmail.com>
2017-05-18 22:43:11 -0400

    Comments explaing program

--- Chapter 1/c1_13c.cc
+++ Chapter 1/c1_13c.cc
@@ -3,17 +3,22 @@
 int main () {
 	int n, m, sum = 0;
 	
+	// Request for input, with contract for input
 	std::cout << "Input two numbers where n < m" << std::endl;
+	// Input is stored in n and m, n being start, m end
 	std::cin >> n >> m;
-
+	
+	// Input is checked for correctness
 	if (n > m){
 		std::cout << "Input is incorrect" << std::endl;
 		return 0;
 	}
 	
+	// Sum is produced
 	for (int count = n; count <= m ; ++count)
 		sum += count;
 	
+	// Sum is outputted
 	std::cout << "The sum from " << n  << " to " 
 		<< m << " is " << sum << std::endl;
 }
