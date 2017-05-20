a7082e10ba961a1495ce1fa9f1e824005cfb8341
Chris <christopher.stojanovski@gmail.com>
2017-05-19 21:34:51 -0400

    Comments

--- Chapter 1/c1_16.cc
+++ Chapter 1/c1_16.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 
 int main () {
+	// Integers holding the inputted value and running sum
 	int val = 0, sum = 0;
 	
 	std::cout << "Input a list of numbers to sum" << std::endl;
 	
+	// Numbers are added while valid input is found
 	while (std::cin >> val) 
 		sum+= val;
-
+	
+	// Total sum is outputted
 	std::cout << "The sum of the inputted numbers is: " << sum << std::endl;
 }
